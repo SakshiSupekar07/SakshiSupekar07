fad01058430a0483bb41fbf6dc013373eabc21a8
SakshiSupekar07 <supekarsakshj07@gmail.com>
2024-09-06 13:05:06 +0530

    Add files via upload

---
+++ A.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#define MAXROW 10
+#define MAXCOL 10
+
+int main()
+{
+    int A[MAXROW][MAXCOL];
+    int B[MAXROW][MAXCOL];
+    int r, c;
+    printf("enter the rows");
+    scanf("%d", &r);
+    printf("enter cls");
+    scanf("%d", &c);
+    printf("enter matrix elements");
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("enter element [%d%d]:", i, j);
+            scanf("%d", &A[i][j]);
+        }
+    }
+    printf("matrix is\n");
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d\t", A[i][j]);
+        }
+
+        printf("\n");
+    }
+    int S=5;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            B[i][j]=A[i][j]+A[i][j];
+        }
+    }
+    printf("addition is\n");
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+
+            printf("%d\t", B[i][j]);
+            
+
+        }
+         printf("\n");
+         
+    }
+    return 0;
+}
+     
+    
